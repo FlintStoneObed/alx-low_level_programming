9ed9499294c35a11d182b1e9764321fd5974768c
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-10 16:03:31 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - Entry Point
+ *
+ * This program prints or outputs "_putchar"
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	char ch[] = "_putchar";
+	int length = strlen(ch);
+
+	int i;
+
+	for (i = 0; i < length; i++)
+	{
+		_putchar(ch[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
