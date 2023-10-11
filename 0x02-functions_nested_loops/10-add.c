484c21893e92a87696a2e5345d73153548d8b84a
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-10 21:49:02 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two integers
+ *
+ * @x: two integer arguments
+ * @y: two integer arguments
+ *
+ * Return: sum
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
