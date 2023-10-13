650a2046c3ab4e5f1fd9334cae64bfb37e7bd354
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-12 18:42:48 -0700

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,23 @@
 #include "main.h"
-
 /**
- * more_numbers - print 10 times the numbers, from 0 to 14
- *
- * Return: void
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  */
 
 void more_numbers(void)
 {
-	int i, j;
+	char i;
+	int j;
 
-	for (i = 1; i <= 10; i++)
+	for (j = 0; j <= 10; j++)
 	{
-		for (j = 0; j <= 14; j++)
-
-			if (j >= 10)
-				_putchar('1');
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 0)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
