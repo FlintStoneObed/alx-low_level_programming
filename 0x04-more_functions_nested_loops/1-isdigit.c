57296d06a79061dc91b2bf19ae071d0a00bb7326
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-12 18:21:48 -0700

    update

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,14 +3,14 @@
 /**
  * main - checks for a digit 0 through 9
  * @c: char to be checked
- * 
+ *
  * Return: Always 0.
  */
 
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-		retrun (1);
+		retrun(1);
 	else
 		return (0);
 }
