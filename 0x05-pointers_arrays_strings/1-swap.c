d2ff2de54b01e51159eb1e5cd6b2fbdb046d7c7e
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-16 13:56:38 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers
+ *
+ * @a: int to swap
+ * @b: int to swap
+ *
+ * Return: Success
+ */
+
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
