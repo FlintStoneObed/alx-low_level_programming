3de44b7d6f8fabe9f7671df010c9b3b89ce3e53e
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-19 04:43:54 -0700

    3-strcmp.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,24 +2,18 @@
 
 /**
  * _strcmp - compare two strings
- * @dest: input value
- * @s1: input value
- * @s2: input value
- * Return: s1[i] - s2[i]
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: 0 if @s1 and @s2 are equal, a negative value if @s1 is
+ * less than @s2, a positive value if @s1 is greater than @s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	i = 0
-		while (s1[i] != '\0' && s2[i] != '\0')
-		{
-			if (s1[i] != s2[i])
-			{
-				return (s1[i] - s2[i]);
-			}
-			i++;
-		}
-		return (0);
+	for (; *s1 == *s2; s1++, s2++)
+	{
+		if (!*s1)
+			return (0); /* Both strings are equal */
+	}
+	return (*s1 - *s2);
 }
