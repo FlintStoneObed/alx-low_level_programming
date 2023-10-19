df8a33ca120df8dee8f666467c43f928528ac612
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-19 07:16:42 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,17 +8,17 @@
 
 char *rot13(char *x)
 {
-	unt i, j;
+	int i, j;
 
 	char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char *b = "NOPQRSTUVQXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for i = 0; x[i] != '\0'; i++)
+	for (i = 0; x[i] != '\0'; i++)
 		for (j = 0; a[j] != '\0'; j++)
 			if (x[i] == a[j])
 			{
 				x[i] = b[j];
 				break;
 			}
-	return (s);
+	return (x);
 }
