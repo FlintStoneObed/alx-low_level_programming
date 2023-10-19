03611d53e8741c3522816dd261498d41dca16145
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-19 07:03:38 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @x: the string to encode
+ * Return: x
+ */
+
+char *rot13(char *x)
+{
+	unt i, j;
+
+	char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *b = "NOPQRSTUVQXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for i = 0; x[i] != '\0'; i++)
+		for (j = 0; a[j] != '\0'; j++)
+			if (x[i] == a[j])
+			{
+				x[i] = b[j];
+				break;
+			}
+	return (s);
+}
