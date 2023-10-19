9b26e97137532740ae2dad74dcda3522a3e1edac
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-19 04:21:08 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,8 @@ char *_strcat(char *dest, char *src)
 {
 	int len, i;
 
-	for (len = 0; dest[len] != '\0'; len++);
+	for (len = 0; dest[len] != '\0'; len++)
+		;
 
 	for (i = 0; src[i] != '\0'; i++)
 		dest[len++] = src[i];
