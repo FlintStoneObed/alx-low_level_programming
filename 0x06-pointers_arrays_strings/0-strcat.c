0f6f0700590f8b2d158d09172814c363de945610
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-19 04:14:44 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,27 +4,18 @@
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- * Return: void
+ * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int len, i;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j]
-		i++;
-		j++;
-	}
+	for (len = 0; dest[len] != '\0'; len++);
 
-	dest[i] = '\0';
+	for (i = 0; src[i] != '\0'; i++)
+		dest[len++] = src[i];
+	dest[len] = '\0';
+
 	return (dest);
 }
