ffe9e63b26febfaffad8ec8a54a7cb6b5dadd43f
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-23 15:23:17 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locate character in string
+ * @s: string
+ * @c: char to search
+ * Return: A pointer to the matched character or NULL if the character
+ * is not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	if (s == NULL)
+		return (NULL);
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
