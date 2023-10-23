8eac776efa8dc03a4f4ad223704bcd6caa48ede8
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-23 15:32:13 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,6 +4,10 @@
  * _strchr - locate character in string
  * @s: string
  * @c: character to search
+ *
+ * Description: The _strchr() function returns a pointer to the first
+ * occurrence of the character @c in the string @s.
+ *
  * Return: A pointer to the matched character or NULL if the character
  * is not found.
  */
