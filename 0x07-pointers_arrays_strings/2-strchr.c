392edf239e94d23c6fc3eecacebf1fcfb97c509d
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-23 15:34:32 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,31 +1,30 @@
 #include "main.h"
 
 /**
- * _strchr - locate character in string
+ * _strchr - locates a char in a string
+ *
  * @s: string
- * @c: character to search
+ * @c: char to be located
  *
- * Description: The _strchr() function returns a pointer to the first
- * occurrence of the character @c in the string @s.
- *
- * Return: A pointer to the matched character or NULL if the character
- * is not found.
+ * Return: pointer to @s
  */
+
 char *_strchr(char *s, char c)
 {
-	/* handle empty string */
-	if (s == NULL)
-		return (NULL);
 
 	while (*s != '\0')
 	{
 		if (*s == c)
-			return (s); /* match found */
+		{
+			return (s);
+		}
 		s++;
 	}
-	/* return a pointer to the null byte if 'c' is a null byte. */
-	if (c == '\0')
-		return (s);
 
-	return (NULL); /* no match found or end of string - return NULL */
+	if (c == '\0')
+	{
+		return (s);
+	}
+
+	return (NULL);
 }
