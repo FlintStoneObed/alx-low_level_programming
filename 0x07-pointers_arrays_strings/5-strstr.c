a9aa95a3b75f45af17cde92171f44377396f69ee
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-23 16:15:09 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,18 +7,21 @@
  * Return: a pointer to the beginning of the substring,
  * or NULL if the substring is not found.
  */
+
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
 	for (i = 0; haystack[i]; i++)
 	{
-		j + 0;
+		j = 0;
+		/* keep searching so long as characters in strings are equal */
 		while (haystack[i + j] == needle[j] && needle[j])
 			j++;
 
+		/* end of substring reached - means there was a full match */
 		if (!needle[j])
 			return (&haystack[i]);
 	}
-	return (NULL);
+	return (NULL); /* no match found */
 }
