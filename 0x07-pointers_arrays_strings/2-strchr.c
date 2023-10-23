a157bb9b4bf5c9b534d559d0c8526ed19ceb358c
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-23 15:28:17 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,24 +3,25 @@
 /**
  * _strchr - locate character in string
  * @s: string
- * @c: char to search
+ * @c: character to search
  * Return: A pointer to the matched character or NULL if the character
  * is not found.
  */
-
 char *_strchr(char *s, char c)
 {
+	/* handle empty string */
 	if (s == NULL)
 		return (NULL);
 
 	while (*s != '\0')
 	{
 		if (*s == c)
-			return (s);
+			return (s); /* match found */
 		s++;
 	}
+	/* return a pointer to the null byte if 'c' is a null byte. */
 	if (c == '\0')
 		return (s);
 
-	return (NULL);
+	return (NULL); /* no match found or end of string - return NULL */
 }
