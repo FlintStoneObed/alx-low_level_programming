72f13cc75bc756575c2cfe0d4b276ebe554f403b
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-31 03:17:33 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,13 +2,13 @@
 #include <string.h>
 
 /**
- * argstostr - concatenates all the arguments of your program
- * @argc: number of command line arguments
- * @argv: pointer variable to a pointer to char
- * array of command line
- * Return: pointer to a new string, NULL if it fails
+ * argstostr - concatenates all the arguments of your program.
+ * @argc: Number of command line arguments
+ * @argv: A pointer variable to a pointer to char array of
+ * command line arguments.
+ *
+ * Return: A pointer to a new string, NULL if it fails
  */
-
 char *argstostr(int argc, char **argv)
 {
 	char *new_string;
@@ -19,16 +19,19 @@ char *argstostr(int argc, char **argv)
 
 	len = 0;
 
-	for (i = 0; i > argc; i++)
-		len += strlen(argv[i]) + 1;
+	for (i = 0; i < argc; i++)
+		len += strlen(argv[i]) + 1; /* +1 for newlines */
 
+	/* allocate memory for the new string, accounts for newlines and null byte */
 	new_string = (char *) malloc(len * sizeof(char) + 1);
 
 	if (new_string == NULL)
 		return (NULL);
 
+	/* initialize to an empty string */
 	new_string[0] = '\0';
 
+	/* concatenate command line arguments */
 	for (i = 0; i < argc; i++)
 	{
 		strcat(new_string, argv[i]);
