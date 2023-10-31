1c527ae150c455eedd2726f9aa19b71f54d12afd
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-10-31 02:01:32 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: width of grid
+ * @height: height of grid
+ * Return: pointer to the 2 dimension of integers, NULL if it fails
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, col;
+
+	if (height <= 0 || widthe <= 0)
+		return (NULL);
+
+	grid = (int **) malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+	{
+		grid[col] = (int *) malloc(width * sizeof(int));
+
+		if (grid[col] == NULL)
+		{
+			while (col >= 0)
+			{
+				free(grid[col]);
+				col--;
+			}
+			free(grid);
+			return (NULL);
+		}
+		memset(grid[col], 0, width * sizeof(int));
+	}
+	return (grid);
+}
