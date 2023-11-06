de5c17bd82aa30b8eff64f98daa7fd8ebf867fbd
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-11-06 14:00:51 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - blueprint for dog entity
+ * @name: name of dog
+ * @owener: name of dog's owner
+ * @age: dog's age
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
