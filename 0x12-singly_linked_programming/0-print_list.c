f86a1c4bdbdf63e766e539c17e5c6bf9dc6c71c6
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-11-16 13:17:09 -0800

    update

--- 0x12-singly_linked_programming/0-print_list.c
+++ 0x12-singly_linked_programming/0-print_list.c
@@ -1,69 +1,29 @@
 #include "lists.h"
 
 /**
- * reverse_string - Reverses a null-terminated string in place.
- * @str: The string to be reversed.
+ * print_list - prints all the element of a list_t list
+ * @head: the head node
+ *
+ * Return: the number of nodes
  */
-
-void reverse_string(char *str)
+size_t print_list(const list_t *head)
 {
-size_t i = 0;
-size_t j = 0;
+	size_t node_count = 0;
 
-while (str[j] != '\0')
-j++;
+	if (head == NULL)
+	{
+		return (0);
+	}
 
-j--;
+	while (head != NULL)
+	{
+		if (head->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", head->len, head->str);
+		node_count++;
+		head = head->next;
+	}
 
-while (i < j)
-{
-char temp = str[i];
-str[i] = str[j];
-str[j] = temp;
-i++;
-j--;
-}
-}
-
-/**
- * print_list - Prints all the elements of a list_t list.
- * @h: Pointer to the head of the linked list.
- * Return: The number of nodes in the linked list.
- */
-
-size_t print_list(const list_t *h)
-{
-size_t count = 0;
-
-while (h != NULL)
-{
-char len_str[11];
-size_t len_str_len = 0;
-unsigned int len_copy = h->len;
-
-do {
-len_str[len_str_len++] = len_copy % 10 + '0';
-len_copy /= 10;
-} while (len_copy > 0);
-
-len_str[len_str_len] = '\0';
-
-reverse_string(len_str);
-
-write(1, "[", 1);
-write(1, len_str, len_str_len);
-write(1, "] ", 2);
-
-if (h->str != NULL)
-write(1, h->str, h->len);
-else
-write(1, "(nil)", 5);
-
-write(1, "\n", 1);
-
-h = h->next;
-count++;
-}
-
-return (count);
+	return (node_count);
 }
--- 0x12-singly_linked_programming/1-list_len.c
+++ 0x12-singly_linked_programming/1-list_len.c
@@ -1,21 +1,27 @@
 #include "lists.h"
 
 /**
- * list_len - Returns the number of elements in a list_t list.
- * @h: Pointer to the head of the linked list.
- * Return: The number of nodes in the linked list.
+ * list_len - returns the number of elements in a linked list_t list
+ * @head: the head node
+ *
+ * Return: the number of elements in the linked list
  */
-
-size_t list_len(const list_t *h)
-
+size_t list_len(const list_t *head)
 {
-size_t count = 0;
+	size_t node_count = 0;
+	list_t *tmp_head = (list_t *) head;
 
-while (h != NULL)
-{
-h = h->next;
-count++;
-}
+	if (tmp_head == NULL)
+	{
+		return (0); /* list is empty */
+	}
 
-return (count);
+	/* get the number of element in the linked list */
+	while (tmp_head != NULL)
+	{
+		node_count++;
+		tmp_head = tmp_head->next;
+	}
+
+	return (node_count);
 }
