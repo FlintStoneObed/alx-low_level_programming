1c10727435f1b9c9d1ebb398559680cc66b46756
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-11-16 08:31:45 -0800

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: head node
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int size;
+
+	size = 0
+
+		while (h)
+		{
+			if (h->str == NULL)
+				printf("[0] (nil)\n");
+			else
+				printf("[%i] %s\n", h->len, h->str);
+			h = h->next;
+			size += 1;
+		}
+
+	return (size);
+}
