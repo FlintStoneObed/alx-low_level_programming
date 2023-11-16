8a528f0bc62f00b5cc35693c9a1a8d84e8bef6b0
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-11-16 12:31:04 -0800

    update

--- 0x12-singly_linked_programming/lists.h
+++ 0x12-singly_linked_programming/lists.h
@@ -13,12 +13,11 @@
  *
  * Description: singly linked list node structure
  */
-
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -26,8 +25,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-void _free(void **ptr);
-#define safe_free(p) _free((void **) &(p))
+int _putchar( char c);
 
 #endif
