60a5e1a0abb9fdc94274b64c0d62db937bd0e0ca
Obed Kwartekwei Quartey <okquartey@st.ug.edu.gh>
2023-11-27 14:24:42 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,127 +1,52 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-#define BUFFER_SIZE 1024
 
 /**
- * main - Entry point of the program.
- * @program_name: Name of the program
- * Return: Exit status.
+ * main - copies the content of a file to another file.
+ * @argc: the number of arguments passed.
+ * @argv: an array of passed arguments whose size is argc
+ *
+ * Return: 0 on success, otherwise specific error code
  */
-
-void display_usage(char *program_name);
-int open_source_file(char *filename);
-int open_destination_file(char *filename);
-void copy_file_content(int source_fd, int dest_fd);
-void handle_error(char *message, char *filename, int source_fd, int dest_fd);
-
 int main(int argc, char *argv[])
 {
-int source_fd, dest_fd;
+	int fd_r, fd_w, r, a, b;
+	char buf[BUFSIZ];
 
-if (argc != 3)
-{
-display_usage(argv[0]);
-exit(97);
-}
-
-source_fd = open_source_file(argv[1]);
-dest_fd = open_destination_file(argv[2]);
-
-copy_file_content(source_fd, dest_fd);
-
-close(source_fd);
-close(dest_fd);
-
-return (0);
-}
-
-/**
- * display_usage - Displays the correct usage of the program.
- * @program_name: The name of the program.
- */
-
-void display_usage(char *program_name)
-{
-dprintf(2, "Usage: %s file_from file_to\n", program_name);
-}
-
-/**
- * open_source_file - Opens the source file for reading.
- * @filename: The name of the source file.
- * Return: The file descriptor of the opened source file.
- */
-
-int open_source_file(char *filename)
-{
-int source_fd = open(filename, O_RDONLY);
-if (source_fd == -1)
-{
-handle_error("Error: Can't read from file", filename, -1, -1);
-}
-return (source_fd);
-}
-
-/**
- * open_destination_file - Opens the destination file for writing.
- * @filename: The name of the destination file.
- * Return: The file descriptor of the opened destination file.
- */
-
-int open_destination_file(char *filename)
-{
-int dest_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (dest_fd == -1)
-{
-handle_error("Error: Can't write to", filename, -1, -1);
-}
-return (dest_fd);
-}
-
-/**
- * copy_file_content - Copies the content from source file to destination file.
- * @source_fd: File descriptor of the source file.
- * @dest_fd: File descriptor of the destination file.
- */
-
-void copy_file_content(int source_fd, int dest_fd)
-{
-ssize_t bytes_read, bytes_written;
-char buffer[BUFFER_SIZE];
-
-while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
-{
-bytes_written = write(dest_fd, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
-{
-handle_error("Error: Can't write to", "", source_fd, dest_fd);
-}
-}
-
-if (bytes_read == -1)
-{
-handle_error("Error: Can't read from file", "", source_fd, dest_fd);
-}
-}
-
-/**
- * handle_error - Handles errors, displays error message,exits program.
- * @message: The error message.
- * @filename: The relevant filename.
- * @source_fd: File descriptor of the source file.
- * @dest_fd: File descriptor of the destination file.
- */
-
-void handle_error(char *message, char *filename, int source_fd, int dest_fd)
-
-{
-dprintf(2, "%s %s\n", message, filename);
-if (source_fd != -1)
-close(source_fd);
-if (dest_fd != -1)
-close(dest_fd);
-exit(99);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(fd_r, buf, BUFSIZ)) > 0)
+	{
+		if (fd_w < 0 || write(fd_w, buf, r) != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_r);
+			exit(99);
+		}
+	}
+	if (r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	a = close(fd_r);
+	b = close(fd_w);
+	if (a < 0 || b < 0)
+	{
+		if (a < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
+		if (b < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
+		exit(100);
+	}
+	return (0);
 }
